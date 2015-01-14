66c1a8ebf724b1cd494cbc366fb0ab47794a6157
Te Thamrong <profte@gmail.com>
2015-01-14 15:35:52 -0500

    working validator

--- validator.py
+++ validator.py
@@ -2,15 +2,17 @@
 
 It verifies that each line is 
 1) a well-formed json and readable by json.loads
-2) a relation json looks like the one given in the training set
+2) a relation json looks similar to the one given in the training set
 """
 import argparse
 import json 
 
 RELATION_TYPES = ['Explicit', 'Implicit', 'AltLex', 'EntRel', 'NoRel']
-SENSES = ['Temporal.Asynchronous',
+SENSES = ['Temporal.Asynchronous.Precedence',
+		'Temporal.Asynchronous.Succession', 
 		'Temporal.Synchrony', 
-		'Contingency.Cause',
+		'Contingency.Cause.Reason',
+		'Contingency.Cause.Result',
 		'Contingency.Pragmatic cause', 
 		'Contingency.Condition', 
 		'Contingency.Pragmatic condition',
@@ -25,12 +27,13 @@ SENSES = ['Temporal.Asynchronous',
 		'Expansion.Exception',
 		'Expansion.List',
 		'EntRel',
-		'NoRel']
+		]
 
 def validate(file_name):
 	lines = open(file_name)
 	for i, line in enumerate(lines):
 		try:
+			print 'Validating line %s' % i
 			relation = json.loads(line)
 			check_type(relation)	
 			check_sense(relation)
@@ -55,14 +58,18 @@ def check_type(relation):
 	relation_type = relation['Type']
 	if relation_type not in RELATION_TYPES:
 		raise ValueError('Invalid type of %s' % relation_type)
+	if relation_type == 'NoRel':
+		raise ValueError('NoRel should be removed as it is treated as a negative example')
 
 
 def check_sense(relation):
 	if 'Sense' not in relation:
 		raise ValueError('Field \'Sense\' is required but not found')
 	senses = relation['Sense']
-	if not isinstance(senses, list): #or len(senses) != 1:
+	if not isinstance(senses, list): 
 		raise TypeError('Sense field must a list of one element')
+	if len(senses) > 1: 
+		raise TypeError('Sense field must a list of one element. Got %s' % len(senses))
 	sense = senses[0]
 	if sense not in SENSES:
 		raise ValueError('Invalid sense of %s' % sense)
@@ -70,14 +77,24 @@ def check_sense(relation):
 def check_args(relation):
 	if 'Arg1' not in relation:
 		raise ValueError('Field \'Arg1\' is required but not found')
+	else:
+		check_span(relation['Arg1'])
 	if 'Arg2' not in relation:
 		raise ValueError('Field \'Arg2\' is required but not found')
+	else:
+		check_span(relation['Arg2'])
 
 def check_connective(relation):
-	pass
+	if 'Connective' not in relation:
+		raise ValueError('Field \'Arg1\' is required but not found')
+	else:
+		check_span(relation['Connective'])
 
 def check_span(span):
-	pass
+	if 'TokenList' not in span:
+		raise ValueError('Field \'TokenList\' is required but not found')
+	if not isinstance(span['TokenList'], list): 
+		raise TypeError('TokenList field must a list of token indices')
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser('System output format validator')
